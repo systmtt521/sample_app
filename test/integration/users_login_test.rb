a18f8964a69596c3ed99ed32116e85cd9b30f210
tomita <systmtt@gmail.com>
2020-02-26 08:34:26 +0000

    Finish user edit,update, index, and destroy actions

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -50,7 +50,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 
-   test "login with remembering" do
+  test "login with remembering" do
     log_in_as(@user, remember_me: '1')
     assert_equal cookies['remember_token'], assigns(:user).remember_token
   end
